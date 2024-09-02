6d86906eb6380bcaec29eb864a2a9fdc136b41ed
Yuzo <victoryuzofb4@gmail.com>
2024-09-02 18:22:05 -0300

    third commit

--- SumOfTwoIntegerWithout+or-.py
+++ SumOfTwoIntegerWithout+or-.py
@@ -1,3 +1,8 @@
+'''
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+'''
+
+
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         array = []
